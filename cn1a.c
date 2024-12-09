910039fbbc63ecea7a44a2b8997050ca10b85c01
Jaguar2703 <pramodshenoy15@gmail.com>
2024-12-09 21:53:06 +0530

    Update cn1a.c

--- cn1a.c
+++ cn1a.c
@@ -1,45 +1,39 @@
 #include<stdio.h>
 #include<string.h>
 
-void receiver();
-char frames[1024];
+#define FRAME_SIZE 1024
+#define MAX_FRAMES 3
 
-int main()
-{
-    int n, len, i;
-    char buffer[256], length[10];
-    printf("How many frames you want to send: ");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
-    {
-        printf("Enter the frame: ");
-        scanf("%s", buffer);
-        len = strlen(buffer);
-        sprintf(length, "%d", len);
-        strcat(frames, length);
-        strcat(frames, buffer);
-    }
-    printf("Frames sent: %s\n", frames);
-    receiver();
-    return 0;
+void sender(const char *data) {
+int length = strlen(data);
+printf("Sender: Sending frame with %d bytes: \"%s\"\n", length, data);
 }
 
-void receiver()
-{
-    int i = 0, framelen, lpvar;
-    printf("\n\nThis is the receiver\n");
-    printf("\nData received is: %s\n", frames);
-    while (frames[i] != '\0')
-    {
-        framelen = frames[i] - '0';
-        i++;
-        printf("\nLength of this frame is: %d\n", framelen);
-        printf("Frame----->");
-        lpvar = i + framelen;
-        while (i < lpvar)
-        {
-            printf("%c", frames[i++]);
-        }
-        printf("\n");
-    }
+void receiver(const char *frame) {
+printf("Receiver: Received frame: \"%s\"\n", frame);
+}
+
+int main() {
+char frames[MAX_FRAMES][FRAME_SIZE];
+int frameCount = 0;
+
+printf("Enter up to %d frames (type 'exit' to finish):\n", MAX_FRAMES);
+
+while (frameCount < MAX_FRAMES) {
+printf("Frame %d: ", frameCount + 1);
+fgets(frames[frameCount], FRAME_SIZE, stdin);
+frames[frameCount][strcspn(frames[frameCount], "\n")] = 0;
+
+if (strcmp(frames[frameCount], "exit") == 0) {
+break;
+}
+
+sender(frames[frameCount]);
+receiver(frames[frameCount]);
+
+frameCount++;
+}
+
+printf("\nTotal frames processed: %d\n", frameCount);
+return 0;
 }
